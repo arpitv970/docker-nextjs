0565af53598c95d26b01f6fe4ac71715a1d8d080
Arpit Verma <arpitv970@gmail.com>
2023-08-11 08:30:09 +0530

    created about && contact page routes along with respective PageLayouts

---
+++ app/about/page.jsx
@@ -0,0 +1,11 @@
+import PageLayout from '@/components/PageLayout';
+
+const About = () => {
+  return (
+  <PageLayout pageHeading={'About Page'} pageDesc={'This is About Page'}>
+    <div>About us Content</div>
+  </PageLayout>
+  )
+}
+
+export default About
---
+++ app/contact/page.jsx
@@ -0,0 +1,11 @@
+import PageLayout from '@/components/PageLayout';
+
+const Contact = () => {
+  return (
+  <PageLayout pageHeading={'Contact Page'} pageDesc={'This is Contact Page'}>
+    <div>Contact form here...</div>
+  </PageLayout>
+  )
+}
+
+export default Contact 
